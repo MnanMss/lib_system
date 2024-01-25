6328ed50db9b2c256a5b8efbcd7a6ffe48e8eaf0
mila <1623892802@qq.com>
2024-01-25 17:51:50 +0800

    v2.0

--- src/test/test/java/com/demp/ApplicationTests.java
+++ src/test/test/java/com/demp/ApplicationTests.java
@@ -1,6 +1,5 @@
-package com.demp;
+package java.com.demp;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
